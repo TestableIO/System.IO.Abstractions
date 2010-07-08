8a23f3e38e57b62096a5d1f14fedf71d28b386cb
Tatham Oddie <tatham@oddie.com.au>
2010-07-08 13:06:19 +1000

    Created solution file.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("System.IO.Abstractions")]
+[assembly: AssemblyDescription("A set of abstractions to help make file system interactions testable.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("System.IO.Abstractions")]
+[assembly: AssemblyCopyright("Copyright © Tatham Oddie 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
