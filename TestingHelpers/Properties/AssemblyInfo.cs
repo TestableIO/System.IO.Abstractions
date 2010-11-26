9b9b00e3fdcba8f883b34979d94121f6b735f064
Tatham Oddie <tatham@oddie.com.au>
2010-11-26 14:41:53 +1100

    Moved version identifiers into SolutionInfo.cs

--- TestingHelpers/Properties/AssemblyInfo.cs
+++ TestingHelpers/Properties/AssemblyInfo.cs
@@ -13,7 +13,4 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-
 [assembly: InternalsVisibleTo("System.IO.Abstractions.TestingHelpers.Tests")]
