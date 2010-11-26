e929bfe85c2714211f689d9ad149588e0658e7bc
Tatham Oddie <tatham@oddie.com.au>
2010-11-26 14:42:41 +1100

    Publishing v1.2.0.0.

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
