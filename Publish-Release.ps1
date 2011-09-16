b08aa625b5fa6e7bd806678aed8106751c400dd6
Tatham Oddie <tatham@oddie.com.au>
2011-09-16 14:12:08 +1000

    Updated publish script to use nuget.exe from source control.

--- Publish-Release.ps1
+++ Publish-Release.ps1
@@ -12,17 +12,18 @@ $ErrorActionPreference = "Stop"
 $PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
 
 $SolutionRoot = Split-Path -Path $PSScriptFilePath -Parent
+$NuGetExe = Join-Path $SolutionRoot -ChildPath ".nuget/nuget.exe"
 
 # Build the NuGet package
 $ProjectPath = Join-Path -Path $SolutionRoot -ChildPath "System.IO.Abstractions\System.IO.Abstractions.csproj"
-& nuget pack $ProjectPath -Prop Configuration=Release -OutputDirectory $SolutionRoot
+& $NuGetExe pack $ProjectPath -Prop Configuration=Release -OutputDirectory $SolutionRoot
 if (-not $?)
 {
 	throw "The NuGet process returned an error code."
 }
 
 $ProjectPath = Join-Path -Path $SolutionRoot -ChildPath "TestingHelpers\TestingHelpers.csproj"
-& nuget pack $ProjectPath -Prop Configuration=Release -OutputDirectory $SolutionRoot
+& $NuGetExe pack $ProjectPath -Prop Configuration=Release -OutputDirectory $SolutionRoot
 if (-not $?)
 {
 	throw "The NuGet process returned an error code."
@@ -32,14 +33,14 @@ if (-not $?)
 if ($Push)
 {
 	$NuPkgPath = Join-Path -Path $SolutionRoot -ChildPath "System.IO.Abstractions.$ReleaseVersionNumber.nupkg"
-	& nuget push $NuPkgPath
+	& $NuGetExe push $NuPkgPath
 	if (-not $?)
 	{
 		throw "The NuGet process returned an error code."
 	}
 
 	$NuPkgPath = Join-Path -Path $SolutionRoot -ChildPath "System.IO.Abstractions.TestingHelpers.$ReleaseVersionNumber.nupkg"
-	& nuget push $NuPkgPath
+	& $NuGetExe push $NuPkgPath
 	if (-not $?)
 	{
 		throw "The NuGet process returned an error code."
