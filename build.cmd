ef610b09b3ca856af9b18abe82143b0b2143a733
Eric Newton <ericnewton76@gmail.com>
2013-03-14 12:25:28 -0400

    Change build.cmd to not resize window

--- build.cmd
+++ build.cmd
@@ -5,13 +5,12 @@ ECHO.
 REM ensure _Build_Output directory exists
 mkdir Build 2>NUL
 
-ECHO.
 ECHO Checking Tools
 cd Tools
-..\.nuget\Nuget install packages.config -excludeversion
+Nuget install packages.config -excludeversion
 cd ..
 
 ECHO.
 ECHO.
-powershell -Command "& { [Console]::WindowWidth = 150; [Console]::WindowHeight = 50; Start-Transcript -path "./Build/lastbuild.log"; Import-Module ".\Tools\psake\tools\psake.psm1"; Invoke-psake .\build-psake.ps1 %*; Stop-Transcript; }"
+powershell -Command "& { Start-Transcript -path "./Build/lastbuild.log"; Import-Module ".\Tools\psake\tools\psake.psm1"; Invoke-psake .\build-psake.ps1 %*; Stop-Transcript; }"
 pause
