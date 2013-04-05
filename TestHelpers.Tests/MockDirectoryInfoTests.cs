5954d5c9ebaa7afabb209a2d76f81133c68adbc4
Dirk Rombauts <dirk.rombauts@questmaster.net>
2013-04-05 14:04:41 +0200

    Implementing MockDirectoryInfo.Extension

---
+++ TestHelpers.Tests/MockDirectoryInfoTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace System.IO.Abstractions.TestingHelpers.Tests
+{
+    [TestFixture]
+    public class MockDirectoryInfoTests
+    {
+        [Test]
+        public void MockDirectoryInfo_GetExtension_ShouldReturnEmptyString()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
+            var directoryInfo = new MockDirectoryInfo(fileSystem, @"c:\temp");
+
+            // Act
+            var result = directoryInfo.Extension;
+
+            // Assert
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [Test]
+        public void MockDirectoryInfo_GetExtensionWithTrailingSlash_ShouldReturnEmptyString()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>());
+            var directoryInfo = new MockDirectoryInfo(fileSystem, @"c:\temp\");
+
+            // Act
+            var result = directoryInfo.Extension;
+
+            // Assert
+            Assert.AreEqual(string.Empty, result);
+        }
+    }
+}
