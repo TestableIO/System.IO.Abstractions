17336f3fc94ec45fca71a8fcbcd2426a89942f82
KallDrexx <me@mshapiro.net>
2014-02-21 20:00:51 -0500

    Created FileSystemWatcherFactory class to to make it easier to test code that needs to generate file system watchers

---
+++ System.IO.Abstractions/IFileSystemWatcherFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.IO.Abstractions
+{
+    public interface IFileSystemWatcherFactory
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileSystemWatcherBase"/> class, which acts as a wrapper for a FileSystemWatcher
+        /// </summary>
+        /// <returns></returns>
+        FileSystemWatcherBase CreateNew();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileSystemWatcherBase"/> class, which acts as a wrapper for a FileSystemWatcher
+        /// </summary>
+        /// <param name="path">Path to generate the FileSystemWatcherBase for</param>
+        /// <returns></returns>
+        FileSystemWatcherBase FromPath(string path);
+    }
+}
