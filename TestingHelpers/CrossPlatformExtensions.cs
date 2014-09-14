d64f35fe58e7ea581275cec8766341492c881593
Ilker Cetinkaya <ilker@ilker.de>
2014-09-14 22:37:06 +0200

    Add preliminary Win/Nix switch

---
+++ TestingHelpers/CrossPlatformExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace System.IO.Abstractions.TestingHelpers
+{
+    public static class XFS
+    {
+        public static string ForWin(string path)
+        {
+            return path;
+        }
+
+        public static string ForUnix(this string path, string unixPath)
+        {
+            int p = (int) Environment.OSVersion.Platform;
+            bool isUnix = (p == 4) || (p == 6) || (p == 128);
+
+            return isUnix ? unixPath : path;
+        }
+    }
+}
