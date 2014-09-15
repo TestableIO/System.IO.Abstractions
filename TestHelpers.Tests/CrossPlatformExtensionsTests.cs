f5007a78c93a45a2cfa0908a1a16eaea49a9f6c2
Ilker Cetinkaya <ilker@ilker.de>
2014-09-15 13:32:46 +0200

    Decide to go for a different xplat path strategy

---
+++ TestHelpers.Tests/CrossPlatformExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace System.IO.Abstractions.TestingHelpers.Tests
+{
+    [TestFixture]
+    public class CrossPlatformExtensionsTests
+    {
+        [Test]
+        public void Should_Convert_Backslashes_To_Slashes_On_Unix()
+        {
+            Assert.AreEqual("/test/", XFS.Path(@"\test\", () => true));
+        }
+
+        [Test]
+        public void Should_Remove_Drive_Letter_On_Unix()
+        {
+            Assert.AreEqual("/test/", XFS.Path(@"c:\test\", () => true));
+        }
+    }
+}
--- TestingHelpers/CrossPlatformExtensions.cs
+++ TestingHelpers/CrossPlatformExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace System.IO.Abstractions.TestingHelpers
 {
@@ -15,6 +16,26 @@ namespace System.IO.Abstractions.TestingHelpers
             bool isUnix = (p == 4) || (p == 6) || (p == 128);
 
             return isUnix ? unixPath : path;
+        }
+
+        public static string Path(string path, Func<bool> isUnixF = null)
+        {
+            if (isUnixF == null)
+            {
+                isUnixF = () =>
+                {
+                    int p = (int)Environment.OSVersion.Platform;
+                    return (p == 4) || (p == 6) || (p == 128);
+                };
+            }
+                
+            if (isUnixF())
+            {
+                path = Regex.Replace(path, @"^[a-zA-Z]:(?<path>.*)$", "${path}");
+                path = path.Replace(@"\", "/");
+            }
+
+            return path;
         }
     }
 }
