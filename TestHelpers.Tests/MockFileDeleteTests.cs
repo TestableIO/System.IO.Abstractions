ff112378ced8aec95a6f207da0c213761d50409e
Cameron MacFarland <distantcam@gmail.com>
2015-03-11 20:32:15 +0800

    Fixed #83 - Changed stream flushing to not re-add the file if it doesn't exist

---
+++ TestHelpers.Tests/MockFileDeleteTests.cs
@@ -0,0 +1,23 @@
+﻿namespace System.IO.Abstractions.TestingHelpers.Tests
+{
+    using NUnit.Framework;
+
+    public class MockFileDeleteTests
+    {
+        [Test]
+        public void MockFile_Delete_ShouldDeleteFile()
+        {
+            var fileSystem = new MockFileSystem();
+            var path = "C:\\test";
+            var directory = fileSystem.Path.GetDirectoryName(path);
+            fileSystem.AddFile(path, new MockFileData("Bla"));
+
+            var fileCount1 = fileSystem.Directory.GetFiles(directory, "*").Length;
+            fileSystem.File.Delete(path);
+            var fileCount2 = fileSystem.Directory.GetFiles(directory, "*").Length;
+
+            Assert.AreEqual(1, fileCount1, "File should have existed");
+            Assert.AreEqual(0, fileCount2, "File should have been deleted");
+        }
+    }
+}
