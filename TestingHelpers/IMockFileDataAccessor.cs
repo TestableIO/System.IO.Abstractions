fe44fc56046194d610432abe06d304db52cc2f17
Manuel Pfemeter <manuel.pfemeter@gmx.de>
2016-06-19 17:35:40 +0200

    added documentation

--- TestingHelpers/IMockFileDataAccessor.cs
+++ TestingHelpers/IMockFileDataAccessor.cs
@@ -2,6 +2,9 @@
 
 namespace System.IO.Abstractions.TestingHelpers
 {
+    /// <summary>
+    /// Provides access to the file system storage.
+    /// </summary>
     public interface IMockFileDataAccessor
     {
         /// <summary>
@@ -29,9 +32,22 @@ namespace System.IO.Abstractions.TestingHelpers
         /// <param name="path">The file to check. </param>
         /// <returns><see langword="true"/> if the file exists; otherwise, <see langword="false"/>.</returns>
         bool FileExists(string path);
+
+        /// <summary>
+        /// Gets all unique paths of all files and directories.
+        /// </summary>
         IEnumerable<string> AllPaths { get; }
+
+        /// <summary>
+        /// Gets the paths of all files.
+        /// </summary>
         IEnumerable<string> AllFiles { get; }
+
+        /// <summary>
+        /// Gets the paths of all directories.
+        /// </summary>
         IEnumerable<string> AllDirectories { get; }
+
         DirectoryBase Directory { get; }
         IFileInfoFactory FileInfo {get; }
         PathBase Path { get; }
