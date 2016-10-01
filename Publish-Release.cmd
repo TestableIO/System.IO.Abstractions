0945216cdfdfe575971e8ffe5181c3d9d061bb3b
Tatham Oddie <tatham@oddie.com.au>
2016-10-01 12:30:47 +1000

    Purge old NuGet pack approach

--- Publish-Release.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set tempscript=%temp%\%~n0.%random%.ps1
-echo $ErrorActionPreference="Stop" >"%tempscript%"
-echo ^& "%~dpn0.ps1" %* >>"%tempscript%"
-powershell.exe -command "& \"%tempscript%\""
-set errlvl=%ERRORLEVEL%
-del "%tempscript%"
-exit /b %errlvl%
