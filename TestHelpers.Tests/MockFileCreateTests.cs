a6a8d3fca5df109cfb48f8e66320b5f4e46ed1df
Florian Greinacher <florian@greinacher.de>
2018-07-04 22:29:57 +0200

    Fix newly added tests

--- TestHelpers.Tests/MockFileCreateTests.cs
+++ TestHelpers.Tests/MockFileCreateTests.cs
@@ -18,7 +18,7 @@ namespace System.IO.Abstractions.TestingHelpers.Tests
         {
             string fullPath = XFS.Path(@"c:\something\demo.txt");
             var fileSystem = new MockFileSystem();
-            fileSystem.AddDirectory(@"c:\something");
+            fileSystem.AddDirectory(XFS.Path(@"c:\something"));
 
             var sut = new MockFile(fileSystem);
 
@@ -34,7 +34,7 @@ namespace System.IO.Abstractions.TestingHelpers.Tests
         {
             string fullPath = XFS.Path(@"c:\something\demo.txt");
             var fileSystem = new MockFileSystem();
-            fileSystem.AddDirectory(@"c:\something");
+            fileSystem.AddDirectory(XFS.Path(@"c:\something"));
             var data = new UTF8Encoding(false).GetBytes("Test string");
 
             var sut = new MockFile(fileSystem);
@@ -54,7 +54,7 @@ namespace System.IO.Abstractions.TestingHelpers.Tests
         {
             string path = XFS.Path(@"c:\some\file.txt");
             var fileSystem = new MockFileSystem();
-            fileSystem.AddDirectory(@"c:\some");
+            fileSystem.AddDirectory(XFS.Path(@"c:\some"));
 
             var mockFile = new MockFile(fileSystem);
 
--- TestHelpers.Tests/MockFileTests.cs
+++ TestHelpers.Tests/MockFileTests.cs
@@ -545,7 +545,7 @@ namespace System.IO.Abstractions.TestingHelpers.Tests
         {
             string filepath = XFS.Path(@"c:\something\doesnt\exist.txt");
             var filesystem = new MockFileSystem(new Dictionary<string, MockFileData>());
-            filesystem.AddDirectory(@"c:\something\doesnt");
+            filesystem.AddDirectory(XFS.Path(@"c:\something\doesnt"));
 
             var stream = filesystem.File.AppendText(filepath);
 
