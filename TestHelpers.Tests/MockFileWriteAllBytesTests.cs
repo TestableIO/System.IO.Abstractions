27a8ac6a6618b80c71fe5ea452c5471567b219a5
John Reese <jpreese@mtu.edu>
2018-07-07 20:46:53 -0400

    Throw exception when illegal characters are in the path

--- TestHelpers.Tests/MockFileWriteAllBytesTests.cs
+++ TestHelpers.Tests/MockFileWriteAllBytesTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-
 using NUnit.Framework;
 using XFS = System.IO.Abstractions.TestingHelpers.MockUnixSupport;
 
@@ -10,95 +9,82 @@ namespace System.IO.Abstractions.TestingHelpers.Tests
         [Test]
         public void MockFile_WriteAllBytes_ShouldThrowDirectoryNotFoundExceptionIfPathDoesNotExists()
         {
-            // Arrange
             var fileSystem = new MockFileSystem();
             string path = XFS.Path(@"c:\something\file.txt");
             var fileContent = new byte[] { 1, 2, 3, 4 };
 
-            // Act
             TestDelegate action = () => fileSystem.File.WriteAllBytes(path, fileContent);
 
-            // Assert
             Assert.Throws<DirectoryNotFoundException>(action);
         }
 
         [Test]
         public void MockFile_WriteAllBytes_ShouldWriteDataToMemoryFileSystem()
         {
-            // Arrange
             string path = XFS.Path(@"c:\something\demo.txt");
             var fileSystem = new MockFileSystem();
             var fileContent = new byte[] { 1, 2, 3, 4 };
             fileSystem.AddDirectory(XFS.Path(@"c:\something"));
 
-            // Act
             fileSystem.File.WriteAllBytes(path, fileContent);
 
-            // Assert
-            Assert.AreEqual(
-                fileContent,
-                fileSystem.GetFile(path).Contents);
+            Assert.AreEqual(fileContent, fileSystem.GetFile(path).Contents);
         }
 
         [Test]
         public void MockFile_WriteAllBytes_ShouldThrowAnUnauthorizedAccessExceptionIfFileIsHidden()
         {
-            // Arrange
             string path = XFS.Path(@"c:\something\demo.txt");
             var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
-                {
-                    { path, new MockFileData("this is hidden") },
-                });
+            {
+                { path, new MockFileData("this is hidden") },
+            });
             fileSystem.File.SetAttributes(path, FileAttributes.Hidden);
 
-            // Act
             TestDelegate action = () => fileSystem.File.WriteAllBytes(path, new byte[] { 123 });
 
-            // Assert
             Assert.Throws<UnauthorizedAccessException>(action, "Access to the path '{0}' is denied.", path);
         }
 
-#if NET40
         [Test]
-#endif
         public void MockFile_WriteAllBytes_ShouldThrowAnArgumentExceptionIfContainsIllegalCharacters()
         {
-            // Arrange
             var fileSystem = new MockFileSystem();
 
-            // Act
             TestDelegate action = () => fileSystem.File.WriteAllBytes("<<<", new byte[] { 123 });
 
-            // Assert
             Assert.Throws<ArgumentException>(action);
         }
 
         [Test]
         public void MockFile_WriteAllBytes_ShouldThrowAnArgumentNullExceptionIfContainsIllegalCharacters()
         {
-            // Arrange
             var fileSystem = new MockFileSystem();
 
-            // Act
             TestDelegate action = () => fileSystem.File.WriteAllBytes(null, new byte[] { 123 });
 
-            // Assert
-            var exception = Assert.Throws<ArgumentNullException>(action);
-            Assert.That(exception.Message, Does.StartWith("Path cannot be null."));
-            Assert.That(exception.ParamName, Is.EqualTo("path"));
+            Assert.Throws<ArgumentNullException>(action);
+        }
+
+        [Test]
+        public void MockFile_WriteAllBytes_ShouldThrowAnArgumentNullExceptionIfContainsIllegalCharacters2()
+        {
+            var fileSystem = new MockFileSystem();
+            fileSystem.AddDirectory(@"C:\");
+
+            TestDelegate action = () => fileSystem.File.WriteAllBytes(@"c:\ab*cde.txt", new byte[100]);
+
+            var exception = Assert.Throws<ArgumentException>(action);
         }
 
         [Test]
         public void MockFile_WriteAllBytes_ShouldThrowAnArgumentNullExceptionIfBytesAreNull()
         {
-            // Arrange
             var fileSystem = new MockFileSystem();
             string path = XFS.Path(@"c:\something\demo.txt");
 
-            // Act
             TestDelegate action = () => fileSystem.File.WriteAllBytes(path, null);
 
-            // Assert
             var exception = Assert.Throws<ArgumentNullException>(action);
             Assert.That(exception.Message, Does.StartWith("Value cannot be null."));
             Assert.That(exception.ParamName, Is.EqualTo("bytes"));
