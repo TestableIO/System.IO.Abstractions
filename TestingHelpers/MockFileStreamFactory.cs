46ba25d34a458a6dafbadf48d247d6dd59ad6430
Florian Greinacher <florian@greinacher.de>
2018-07-10 22:45:37 +0200

    Adapt MockFileStreamFactory to MockFileStream changes

--- TestingHelpers/MockFileStreamFactory.cs
+++ TestingHelpers/MockFileStreamFactory.cs
@@ -20,81 +20,97 @@ namespace System.IO.Abstractions.TestingHelpers
 
         public Stream Create(string path, FileMode mode)
         {
-            return new MockFileStream(mockFileSystem, path, mode == FileMode.Append);
+            return new MockFileStream(mockFileSystem, path, GetStreamType(mode));
         }
 
         public Stream Create(string path, FileMode mode, FileAccess access)
         {
-            return new MockFileStream(mockFileSystem, path, mode == FileMode.Append);
+            return new MockFileStream(mockFileSystem, path, GetStreamType(mode, access));
         }
 
         public Stream Create(string path, FileMode mode, FileAccess access, FileShare share)
         {
-            return new MockFileStream(mockFileSystem, path, mode == FileMode.Append);
+            return new MockFileStream(mockFileSystem, path, GetStreamType(mode, access));
         }
 
         public Stream Create(string path, FileMode mode, FileAccess access, FileShare share, int bufferSize)
         {
-            return new MockFileStream(mockFileSystem, path, mode == FileMode.Append);
+            return new MockFileStream(mockFileSystem, path, GetStreamType(mode, access));
         }
 
         public Stream Create(string path, FileMode mode, FileAccess access, FileShare share, int bufferSize, FileOptions options)
         {
-            return new MockFileStream(mockFileSystem, path, mode == FileMode.Append);
+            return new MockFileStream(mockFileSystem, path, GetStreamType(mode, access));
         }
 
         public Stream Create(string path, FileMode mode, FileAccess access, FileShare share, int bufferSize, bool useAsync)
         {
-            return new MockFileStream(mockFileSystem, path, mode == FileMode.Append);
+            return new MockFileStream(mockFileSystem, path, GetStreamType(mode, access));
         }
 
         public Stream Create(string path, FileMode mode, FileSystemRights rights, FileShare share, int bufferSize, FileOptions options, FileSecurity fileSecurity)
         {
-            return new MockFileStream(mockFileSystem, path, mode == FileMode.Append);
+            return new MockFileStream(mockFileSystem, path, GetStreamType(mode));
         }
 
         public Stream Create(string path, FileMode mode, FileSystemRights rights, FileShare share, int bufferSize, FileOptions options)
         {
-            return new MockFileStream(mockFileSystem, path, mode == FileMode.Append);
+            return new MockFileStream(mockFileSystem, path, GetStreamType(mode));
         }
 
         [Obsolete("This method has been deprecated. Please use new Create(SafeFileHandle handle, FileAccess access) instead. http://go.microsoft.com/fwlink/?linkid=14202")]
         public Stream Create(IntPtr handle, FileAccess access)
         {
-            return new MockFileStream(mockFileSystem, handle.ToString(), forAppend: true);
+            return new MockFileStream(mockFileSystem, handle.ToString(), GetStreamType(FileMode.Append, access));
         }
 
         [Obsolete("This method has been deprecated. Please use new Create(SafeFileHandle handle, FileAccess access) instead, and optionally make a new SafeFileHandle with ownsHandle=false if needed. http://go.microsoft.com/fwlink/?linkid=14202")]
         public Stream Create(IntPtr handle, FileAccess access, bool ownsHandle)
         {
-            return new MockFileStream(mockFileSystem, handle.ToString(), forAppend: true);
+            return new MockFileStream(mockFileSystem, handle.ToString(), GetStreamType(FileMode.Append, access));
         }
 
         [Obsolete("This method has been deprecated. Please use new Create(SafeFileHandle handle, FileAccess access, int bufferSize) instead, and optionally make a new SafeFileHandle with ownsHandle=false if needed. http://go.microsoft.com/fwlink/?linkid=14202")]
         public Stream Create(IntPtr handle, FileAccess access, bool ownsHandle, int bufferSize)
         {
-            return new MockFileStream(mockFileSystem, handle.ToString(), forAppend: true);
+            return new MockFileStream(mockFileSystem, handle.ToString(), GetStreamType(FileMode.Append, access));
         }
 
         [Obsolete("This method has been deprecated. Please use new Create(SafeFileHandle handle, FileAccess access, int bufferSize, bool isAsync) instead, and optionally make a new SafeFileHandle with ownsHandle=false if needed. http://go.microsoft.com/fwlink/?linkid=14202")]
         public Stream Create(IntPtr handle, FileAccess access, bool ownsHandle, int bufferSize, bool isAsync)
         {
-            return new MockFileStream(mockFileSystem, handle.ToString(), forAppend: true);
+            return new MockFileStream(mockFileSystem, handle.ToString(), GetStreamType(FileMode.Append, access));
         }
 
         public Stream Create(SafeFileHandle handle, FileAccess access)
         {
-            return new MockFileStream(mockFileSystem, handle.ToString(), forAppend: true);
+            return new MockFileStream(mockFileSystem, handle.ToString(), GetStreamType(FileMode.Append, access));
         }
 
         public Stream Create(SafeFileHandle handle, FileAccess access, int bufferSize)
         {
-            return new MockFileStream(mockFileSystem, handle.ToString(), forAppend: true);
+            return new MockFileStream(mockFileSystem, handle.ToString(), GetStreamType(FileMode.Append, access));
         }
 
         public Stream Create(SafeFileHandle handle, FileAccess access, int bufferSize, bool isAsync)
         {
-            return new MockFileStream(mockFileSystem, handle.ToString(), forAppend: true);
+            return new MockFileStream(mockFileSystem, handle.ToString(), GetStreamType(FileMode.Append, access));
+        }
+
+        private static MockFileStream.StreamType GetStreamType(FileMode mode, FileAccess access = FileAccess.ReadWrite)
+        {
+            if (access == FileAccess.Read)
+            {
+                return MockFileStream.StreamType.READ;
+            }
+            else if (mode == FileMode.Append) 
+            {
+                return MockFileStream.StreamType.APPEND;
+            }
+            else
+            {
+                return MockFileStream.StreamType.WRITE;
+            }
         }
     }
 }
