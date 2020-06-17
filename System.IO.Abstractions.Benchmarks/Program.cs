0a8856c5fbfcf022709905aacf7cbcad3f0cdc5e
Paulino Padial <paulino.padial@gmail.com>
2020-06-17 10:30:36 +0200

    System-IO-Abstractions/System.IO.Abstractions/issues#598 Added benchmarks to project

---
+++ System.IO.Abstractions.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace System.IO.Abstractions.Benchmarks
+{
+    using BenchmarkDotNet.Running;
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<FileSystemAbstractionBenchmarks>();
+        }
+    }
+}
