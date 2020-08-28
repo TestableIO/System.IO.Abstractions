00e4b2f3d0472d52aa131cdc5534f55670338645
ilosophiep <soenke@jung-reken.de>
2020-08-28 22:26:59 +0200

    Added tests for Directory.GetParent(string)

---
+++ tests/System.IO.Abstractions.Tests/DirectoryWrapperTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace System.IO.Abstractions.Tests
+{
+    [TestFixture]
+    public class DirectoryWrapperTests
+    {
+        [Test]
+        public void GetParent_ForRootDirectory_ShouldReturnNull()
+        {
+            // Arrange
+            var wrapperFilesystem = new FileSystem();
+
+            // Act
+            var result = wrapperFilesystem.Directory.GetParent(@"C:\");
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void GetParent_ForSimpleDirectory_ShouldReturnParent()
+        {
+            // Arrange
+            var expectedBaseDirectory = @"C:\directory1";
+
+            var wrapperFilesystem = new FileSystem();
+
+            // Act
+            var result = wrapperFilesystem.Directory.GetParent($@"{expectedBaseDirectory}\file1.txt");
+
+            // Assert
+            Assert.AreEqual(expectedBaseDirectory, result.FullName);
+        }
+    }
+}
