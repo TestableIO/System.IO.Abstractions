f3ae9a9dc9f155845fba2cfe6ff5849c56c9e6bc
IloSophiep <soenke@jung-reken.de>
2020-09-11 23:11:55 +0200

    fix: return null when calling DirectoryWrapper.GetParent on the root directory
    Resolves #635

---
+++ tests/System.IO.Abstractions.Tests/DirectoryWrapperTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+
+namespace System.IO.Abstractions.Tests
+{
+    [TestFixture]
+    public class DirectoryWrapperTests
+    {
+        [Test]
+        public void GetParent_ForRootDirectory_ShouldReturnNull()
+        {
+            // Arrange
+            var wrapperFilesystem = new FileSystem();
+            var root = wrapperFilesystem.Directory.GetDirectoryRoot(".");
+
+            // Act
+            var result = wrapperFilesystem.Directory.GetParent(root);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void GetParent_ForSimpleSubfolderPath_ShouldReturnRoot()
+        {
+            // Arrange
+            var wrapperFilesystem = new FileSystem();
+            var root = wrapperFilesystem.Directory.GetDirectoryRoot(".");
+            var subfolder = wrapperFilesystem.Path.Combine(root, "some-folder");
+
+            // Act
+            var result = wrapperFilesystem.Directory.GetParent(subfolder);
+
+            // Assert
+            Assert.AreEqual(root, result.FullName);
+        }
+
+        [Test]
+        public void GetParent_ForSimpleFilePath_ShouldReturnSubfolder()
+        {
+            // Arrange
+            var wrapperFilesystem = new FileSystem();
+            var root = wrapperFilesystem.Directory.GetDirectoryRoot(".");
+            var subfolder = wrapperFilesystem.Path.Combine(root, "some-folder");
+            var file = wrapperFilesystem.Path.Combine(subfolder, "some-file.txt");
+
+            // Act
+            var result = wrapperFilesystem.Directory.GetParent(file);
+
+            // Assert
+            Assert.AreEqual(subfolder, result.FullName);
+        }
+    }
+}
