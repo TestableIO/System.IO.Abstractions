c74750f2abd5347353b648f0b4bb7b0cdef8889c
Florian Greinacher <florian@greinacher.de>
2020-11-05 16:28:42 +0100

    improve naming

--- tests/System.IO.Abstractions.Tests/ApiParityTests.cs
+++ tests/System.IO.Abstractions.Tests/ApiParityTests.cs
@@ -9,73 +9,69 @@ using static System.Reflection.BindingFlags;
 namespace System.IO.Abstractions.Tests
 {
     [TestFixture]
-    public class ApiCompletenessTests
+    public class ApiParityTests
     {
         [Test]
         public void File() =>
-            AssertTypeParity(
+            AssertParity(
                 typeof(System.IO.File),
                 typeof(System.IO.Abstractions.FileBase)
             );
 
         [Test]
         public void FileInfo() =>
-            AssertTypeParity(
+            AssertParity(
                 typeof(System.IO.FileInfo),
                 typeof(System.IO.Abstractions.FileInfoBase)
             );
 
         [Test]
         public void Directory() =>
-            AssertTypeParity(
+            AssertParity(
                 typeof(System.IO.Directory),
                 typeof(System.IO.Abstractions.DirectoryBase)
             );
 
         [Test]
         public void DirectoryInfo() =>
-            AssertTypeParity(
+            AssertParity(
                 typeof(System.IO.DirectoryInfo),
                 typeof(System.IO.Abstractions.DirectoryInfoBase)
             );
 
         [Test]
         public void DriveInfo() =>
-            AssertTypeParity(
+            AssertParity(
                 typeof(System.IO.DriveInfo),
                 typeof(System.IO.Abstractions.DriveInfoBase)
             );
 
         [Test]
         public void Path() =>
-            AssertTypeParity(
+            AssertParity(
                 typeof(System.IO.Path),
                 typeof(System.IO.Abstractions.PathBase)
             );
 
-        private void AssertTypeParity(Type referenceType, Type abstractionType)
+        private void AssertParity(Type referenceType, Type abstractionType)
         {
-            const BindingFlags bindingFlags = Instance | Static | Public | FlattenHierarchy;
-            var expectedMembers = referenceType
-                .GetMembers(bindingFlags)
+            static IEnumerable<string> GetMembers(Type type) => type
+                .GetMembers(bindingAttr: Instance | Static | Public | FlattenHierarchy)
                 .Select(x => x.ToString())
-                .OrderBy(x => x, StringComparer.Ordinal)
+                .OrderBy(x => x, StringComparer.Ordinal);
+            var referenceMembers = GetMembers(referenceType)
                 .Select(x => x.Replace("System.IO.FileStream", "System.IO.Stream"))
                 .Select(x => x.Replace("System.IO.FileSystemInfo", "System.IO.Abstractions.IFileSystemInfo"))
                 .Select(x => x.Replace("System.IO.FileInfo", "System.IO.Abstractions.IFileInfo"))
                 .Select(x => x.Replace("System.IO.DirectoryInfo", "System.IO.Abstractions.IDirectoryInfo"))
                 .Select(x => x.Replace("System.IO.DriveInfo", "System.IO.Abstractions.IDriveInfo"));
-            var implementedMembers = abstractionType
-                .GetMembers(bindingFlags)
-                .Select(x => x.ToString())
-                .OrderBy(x => x, StringComparer.Ordinal)
+            var abstractionMembers = GetMembers(abstractionType)
                 .Where(x => !x.Contains("op_Implicit"))
                 .Where(x => x != "System.IO.Abstractions.IFileSystem get_FileSystem()")
                 .Where(x => x != "System.IO.Abstractions.IFileSystem FileSystem");
-
             var diff = new ApiDiff(
-                implementedMembers.Except(expectedMembers),
-                expectedMembers.Except(implementedMembers)
+                extraMembers: abstractionMembers.Except(referenceMembers),
+                missingMembers: referenceMembers.Except(abstractionMembers)
             );
             Snapshot.Match(diff, SnapshotNameExtension.Create(snapshotSuffix));
         }
