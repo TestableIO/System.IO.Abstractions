cb8cf448bf8e993a75e017e77ad35ac127160432
Valentin Breuß <v.breuss@tig.at>
2022-11-28 20:39:04 +0100

    Fix merge error

---
+++ src/TestableIO.System.IO.Abstractions.TestingHelpers/FileSystemExtensibility.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace System.IO.Abstractions.TestingHelpers
+{
+    internal class FileSystemExtensibility
+    {
+        private readonly Dictionary<string, object> _metadata = new();
+
+        public virtual void StoreMetadata(string key, object value)
+        {
+            _metadata[key] = value;
+        }
+
+        public virtual object RetrieveMetadata(string key)
+        {
+            if (_metadata.TryGetValue(key, out object value))
+            {
+                return value;
+            }
+
+            return default;
+        }
+
+        internal void CopyMetadataFrom(FileSystemExtensibility template)
+        {
+            foreach (var item in template._metadata)
+            {
+                _metadata[item.Key] = item.Value;
+            }
+        }
+    }
+}
