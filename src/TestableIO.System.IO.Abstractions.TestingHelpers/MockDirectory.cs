a2194d7156412c543f1eb0b247c781dea2445214
Greg Tan <greg.tan@firstmode.com>
2023-04-11 10:58:20 +0800

    Removed unused using.

--- src/TestableIO.System.IO.Abstractions.TestingHelpers/MockDirectory.cs
+++ src/TestableIO.System.IO.Abstractions.TestingHelpers/MockDirectory.cs
@@ -2,7 +2,6 @@
 using System.Globalization;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
 using System.Text.RegularExpressions;
 
 namespace System.IO.Abstractions.TestingHelpers
