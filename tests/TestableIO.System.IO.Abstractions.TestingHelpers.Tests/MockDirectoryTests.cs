a19856ea9bfeba18e3dd4b040f0e8e861be563ea
Valentin Breuß <vbreuss@gmail.com>
2024-06-22 07:46:12 +0200

    Add failing test for #1125

--- tests/TestableIO.System.IO.Abstractions.TestingHelpers.Tests/MockDirectoryTests.cs
+++ tests/TestableIO.System.IO.Abstractions.TestingHelpers.Tests/MockDirectoryTests.cs
@@ -879,6 +879,15 @@ namespace System.IO.Abstractions.TestingHelpers.Tests
             Assert.That(Path.GetFileName(result.FullName).StartsWith("foo-"), Is.True);
             Assert.That(result.FullName.Contains(Path.GetTempPath()), Is.True);
         }
+
+        [Test]
+        public void MockDirectory_CreateTempSubdirectory_ShouldStartWithTempPath()
+        {
+            var fileSystem = new MockFileSystem();
+            var result = fileSystem.Directory.CreateTempSubdirectory();
+            var temp = fileSystem.Path.GetTempPath();
+            Assert.That(result.FullName, Does.StartWith(temp));
+        }
 #endif
 
         [Test]
