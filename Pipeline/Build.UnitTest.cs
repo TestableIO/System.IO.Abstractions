1504e5fccaf6d9afd2a8df58277230ae0072bf57
Valentin <valentin.breuss@baur.eu>
2025-02-22 21:46:57 +0100

    Use NUnit in pipeline

--- Pipeline/Build.UnitTest.cs
+++ Pipeline/Build.UnitTest.cs
@@ -1,11 +1,11 @@
+using System.IO;
 using System.Linq;
 using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Tools.Xunit;
-using static Nuke.Common.Tools.Xunit.XunitTasks;
+using Nuke.Common.Tools.NUnit;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 // ReSharper disable AllUnderscoreLocalParameterName
@@ -14,61 +14,66 @@ namespace Build;
 
 partial class Build
 {
-	Target UnitTests => _ => _
-		.DependsOn(DotNetFrameworkUnitTests)
-		.DependsOn(DotNetUnitTests);
+    Target UnitTests => _ => _
+        .DependsOn(DotNetFrameworkUnitTests)
+        .DependsOn(DotNetUnitTests);
 
-	Project[] UnitTestProjects =>
-	[
-		Solution.Tests.TestableIO_System_IO_Abstractions_Wrappers_Tests,
-		Solution.Tests.TestableIO_System_IO_Abstractions_TestingHelpers_Tests,
-		Solution.Tests.TestableIO_System_IO_Abstractions_Parity_Tests,
-	];
+    Project[] UnitTestProjects =>
+    [
+        Solution.Tests.TestableIO_System_IO_Abstractions_Wrappers_Tests,
+        Solution.Tests.TestableIO_System_IO_Abstractions_TestingHelpers_Tests,
+        Solution.Tests.TestableIO_System_IO_Abstractions_Parity_Tests,
+    ];
 
-	Target DotNetFrameworkUnitTests => _ => _
-		.Unlisted()
-		.DependsOn(Compile)
-		.OnlyWhenDynamic(() => EnvironmentInfo.IsWin)
-		.Executes(() =>
-		{
-			string[] testAssemblies = UnitTestProjects
-				.SelectMany(project =>
-					project.Directory.GlobFiles(
-						$"bin/{(Configuration == Configuration.Debug ? "Debug" : "Release")}/net48/*.Tests.dll"))
-				.Select(p => p.ToString())
-				.ToArray();
+    Target DotNetFrameworkUnitTests => _ => _
+        .Unlisted()
+        .DependsOn(Compile)
+        .OnlyWhenDynamic(() => EnvironmentInfo.IsWin)
+        .Executes(() =>
+        {
+            var testAssemblies = UnitTestProjects
+                .SelectMany(project =>
+                    project.Directory.GlobFiles(
+                        $"bin/{(Configuration == Configuration.Debug ? "Debug" : "Release")}/net472/*.Tests.dll"))
+                .Select(p => p.ToString())
+                .ToArray();
 
-			Assert.NotEmpty(testAssemblies.ToList());
+            Assert.NotEmpty(testAssemblies.ToList());
+            
+            foreach (var testAssembly in testAssemblies)
+            {
+                var currentDirectory = Path.GetDirectoryName(testAssembly);
 
-			Xunit2(s => s
-				.SetFramework("net48")
-				.AddTargetAssemblies(testAssemblies)
-			);
-		});
+                NUnitTasks.NUnit3(s => s
+                    .SetInputFiles(testAssemblies)
+                    .SetProcessWorkingDirectory(currentDirectory)
+                );
+            }
+        });
 
-	Target DotNetUnitTests => _ => _
-		.Unlisted()
-		.DependsOn(Compile)
-		.Executes(() =>
-		{
-			string[] excludedFrameworks = ["net48",];
-			DotNetTest(s => s
-					.SetConfiguration(Configuration)
-					.SetProcessEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en-US")
-					.EnableNoBuild()
-					.SetDataCollector("XPlat Code Coverage")
-					.SetResultsDirectory(TestResultsDirectory)
-					.CombineWith(
-						UnitTestProjects,
-						(settings, project) => settings
-							.SetProjectFile(project)
-							.CombineWith(
-								project.GetTargetFrameworks()?.Except(excludedFrameworks),
-								(frameworkSettings, framework) => frameworkSettings
-									.SetFramework(framework)
-									.AddLoggers($"trx;LogFileName={project.Name}_{framework}.trx")
-							)
-					), completeOnFailure: true
-			);
-		});
+    Target DotNetUnitTests => _ => _
+        .Unlisted()
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            string[] excludedFrameworks = ["net48",];
+            DotNetTest(s => s
+                    .SetConfiguration(Configuration)
+                    .SetProcessEnvironmentVariable("DOTNET_CLI_UI_LANGUAGE", "en-US")
+                    .EnableNoBuild()
+                    .SetDataCollector("XPlat Code Coverage")
+                    .SetResultsDirectory(TestResultsDirectory)
+                    .CombineWith(
+                        UnitTestProjects,
+                        (settings, project) => settings
+                            .SetProjectFile(project)
+                            .CombineWith(
+                                project.GetTargetFrameworks()?.Except(excludedFrameworks),
+                                (frameworkSettings, framework) => frameworkSettings
+                                    .SetFramework(framework)
+                                    .AddLoggers($"trx;LogFileName={project.Name}_{framework}.trx")
+                            )
+                    ), completeOnFailure: true
+            );
+        });
 }
